95f449a38ae7b92e82e0d95cb76861ca573143b2
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2023-07-25 12:24:39 +1000

    Add files via upload

---
+++ 02_challenge_boomerang/02_challenge_boomerang.ino
@@ -0,0 +1,29 @@
+/*
+	Program the car to travel at least 1 metre away and return as close as possible to the starting point.
+
+	Author: Phil Legge 26/03/19
+	email: phil.legge@det.nsw.edu.au
+*/
+
+#include "CarBase.h"
+
+//create variables one CarBase object and a boolean value for selection
+	CarBase *myBase;
+	bool flag;
+	
+	void setup(){   // sets up once as the program starts
+		myBase = new CarBase();
+		flag = true;
+	}
+
+	void loop(){ // loops continuously 50 times a second
+
+		if (flag){
+			myBase->driveForward(200,200);
+			delay(1000);
+			flag = false;
+		}else{
+      myBase->driveForward(0,0);
+    }
+	}
+
