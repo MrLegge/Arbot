f672ca9fc3ca97724900807648845e0d1c5aa588
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2023-07-25 12:25:24 +1000

    Add files via upload

---
+++ 04_challenge_triangle/04_challenge_triangle.ino
@@ -0,0 +1,29 @@
+/*
+Program your car to drive travel in an equilateral triangle.
+
+	Author: Phil Legge 26/03/19
+	email: phil.legge@det.nsw.edu.au
+*/
+
+#include "CarBase.h"
+
+//create variables one CarBase object and a boolean value for selection
+	CarBase *myBase;
+	bool flag;
+	
+	void setup(){   // sets up once as the program starts
+		myBase = new CarBase();
+		flag = true;
+	}
+
+	void loop(){ // loops continuously 50 times a second
+
+		if (flag){
+			myBase->driveForward(200,200);
+			delay(1000);
+			flag = false;
+		}else{
+      myBase->driveForward(0,0);
+    }
+	}
+
