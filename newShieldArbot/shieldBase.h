8a39f212883af2f3b1c75a1ffa448ed6f1291d59
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2023-08-06 11:02:26 +1000

    Update shieldBase.h

--- newShieldArbot/shieldBase.h
+++ newShieldArbot/shieldBase.h
@@ -90,28 +90,3 @@ void rotateLeft(int turnSpeed)
   analogWrite(leftMotorSpeedPin, turnSpeed);
   analogWrite(rightMotorSpeedPin, turnSpeed);
 }
-/****************ultraSonic distance function*****************************/
-  int ultraSonicDistance(int trig, int echo){
-    // returns error code or value for use in logic conditions based on offset to object infront
-    long duration;
-    int distance;
-    pinMode(trig, OUTPUT);
-    pinMode(echo, INPUT);
-    digitalWrite(trig, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig, LOW);
-    duration = pulseIn(echo, HIGH);
-    distance = (duration/2) / 29;
-    if (distance >= 300 ){ // return -1 if bigger than 3m
-      return -1;
-    }
-    else if(distance <= 0){ // returns 0 for error or zero results
-      return 0;
-    }
-    else {
-      return distance; // returns a value if test is between 0m and 3m
-    }
-    delay(100);
-  }
