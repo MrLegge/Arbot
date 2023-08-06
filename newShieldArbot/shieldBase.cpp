e48f533071b7074e0777d9293c25ba8965d0640b
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2023-08-06 11:01:03 +1000

    Update shieldBase.cpp

--- newShieldArbot/shieldBase.cpp
+++ newShieldArbot/shieldBase.cpp
@@ -1,1 +1,9 @@
+/*
+  shieldBase.h class library to provide bsics of the robotbase with the new shield
+  
+  Author: Robert Newell + chatGPT  27/7/23
+  Adapted: Phil Legge  6/8/23
+  email: phil.legge@det.nsw.edu.au
+*/
+
 
