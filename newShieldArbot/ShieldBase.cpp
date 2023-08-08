b9897901168aa7542030434672befb91266a6294
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2023-08-08 12:08:03 +1000

    Update ShieldBase.cpp

--- newShieldArbot/ShieldBase.cpp
+++ newShieldArbot/ShieldBase.cpp
@@ -1,5 +1,5 @@
 /*
-  shieldBase.cpp class library to provide basics of the robotbase with the new shield
+  shieldBase.cpp class library to provide basics of the robotbase with the new L298P shield (FunduMoto)
   
   Author: Robert Newell + chatGPT  27/7/23
   Adapted: Phil Legge  6/8/23
