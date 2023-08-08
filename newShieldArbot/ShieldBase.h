a2cdfb1a57bb52dbd0314241d17755367c5e1d7c
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2023-08-08 11:04:35 +1000

    Update ShieldBase.h

--- newShieldArbot/ShieldBase.h
+++ newShieldArbot/ShieldBase.h
@@ -5,86 +5,29 @@
   Adapted: Phil Legge  6/8/23
   email: phil.legge@det.nsw.edu.au
 */
+#ifndef CARBASE_H
+#define CARBASE_H
 
+class ShieldBase{
+  public:
+    const int BUZZER = 4;
+    const int MIN_SPEED = 30;   // Set to minimum PWM value that will make motors turn
+    const int ACCEL_DELAY = 50; // delay between steps when ramping motor speed up or down.
 
+    //  Motor Right
+    int rightMotorSpeedPin, rightMotorDirectionPin;
+    //  Motor Left
+    int leftMotorSpeedPin, leftMotorDirectionPin;
 
-//  Motor Right
-int const rightMotorSpeedPin = 10;
-int const rightMotorDirectionPin = 12;
-//  Motor Left
-int const leftMotorSpeedPin = 11;
-int const leftMotorDirectionPin = 13;
+    ShieldBase();
 
-int const BUZZER = 4;
+    void driveForward(int, int);
+    void driveBackward(int, int);
+    void rotateRight(int);
+    void rotateLeft(int);
 
-int const MIN_SPEED = 30;   // Set to minimum PWM value that will make motors turn
-int const ACCEL_DELAY = 50; // delay between steps when ramping motor speed up or down.
-void Motor(char mot, char dir, int speed)
-{
- 
+  private:
+
 }
 
-
-//Function to drive the car forward with custom left and right motor speeds.
-void driveForward(int leftMotorSpeed, int rightMotorSpeed)
-{
-  // Ensure the motor speeds are within the valid range (0 to 255).
-  leftMotorSpeed = constrain(leftMotorSpeed, 0, 255);
-  rightMotorSpeed = constrain(rightMotorSpeed, 0, 255);
-
-  // Set the directions for forward motion.
-  digitalWrite(leftMotorDirectionPin, HIGH);
-  digitalWrite(rightMotorDirectionPin, HIGH);
-
-  // Set the motor speeds.
-  analogWrite(leftMotorSpeedPin, leftMotorSpeed);
-  analogWrite(rightMotorSpeedPin, rightMotorSpeed);
-}
-
-
-//Function to drive the car backward with custom left and right motor speeds.
-void driveBackward(int leftMotorSpeed, int rightMotorSpeed)
-{
-  // Ensure the motor speeds are within the valid range (0 to 255).
-  leftMotorSpeed = constrain(leftMotorSpeed, 0, 255);
-  rightMotorSpeed = constrain(rightMotorSpeed, 0, 255);
-
-  // Set the directions for backward motion.
-  digitalWrite(leftMotorDirectionPin, LOW);
-  digitalWrite(rightMotorDirectionPin, LOW);
-
-  // Set the motor speeds.
-  analogWrite(leftMotorSpeedPin, leftMotorSpeed);
-  analogWrite(rightMotorSpeedPin, rightMotorSpeed);
-}
-
-// Function to rotate the car right with left wheel forward and right wheel backward.
-void rotateRight(int rotateSpeed)
-{
-  // Ensure the rotation speed is within the valid range (0 to 255).
-  rotateSpeed = constrain(rotateSpeed, 0, 255);
-
-  // Set the directions for rotation.
-  digitalWrite(leftMotorDirectionPin, HIGH);
-  digitalWrite(rightMotorDirectionPin, LOW);
-
-  // Set the motor speeds for rotation.
-  analogWrite(leftMotorSpeedPin, rotateSpeed);
-  analogWrite(rightMotorSpeedPin, rotateSpeed);
-}
-
-
-// Function to rotate the car left with right wheel forward and left wheel backward.
-void rotateLeft(int turnSpeed)
-{
-  // Ensure the turn speed is within the valid range (0 to 255).
-  turnSpeed = constrain(turnSpeed, 0, 255);
-
-  // Set the directions for rotation.
-  digitalWrite(leftMotorDirectionPin, LOW);
-  digitalWrite(rightMotorDirectionPin, HIGH);
-
-  // Set the motor speeds for rotation.
-  analogWrite(leftMotorSpeedPin, turnSpeed);
-  analogWrite(rightMotorSpeedPin, turnSpeed);
-}
+#endif 
